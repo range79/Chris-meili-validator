8380a3035baff0710830e335d5f9fe1a90d93d2a
range79 <darkrange006@gmail.com>
2026-01-27 19:11:49 +0400

    build gradle changed implmanentation-> compile only

--- spring/build.gradle.kts
+++ spring/build.gradle.kts
@@ -12,8 +12,8 @@ repositories {
 dependencies {
     implementation(project(":core"))
 
-    implementation("org.springframework:spring-context:7.0.3")
-    implementation("org.springframework.boot:spring-boot-autoconfigure:4.0.0")
+    compileOnly("org.springframework:spring-context:7.0.3")
+    compileOnly("org.springframework.boot:spring-boot-autoconfigure:4.0.0")
     compileOnly("org.springframework.boot:spring-boot-configuration-processor:4.0.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor:4.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter:6.0.2")
